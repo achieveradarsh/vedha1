1e990126a82b6fbdef56b3a8816cfba4967acb60
achieveradarsh <158289007+achieveradarsh@users.noreply.github.com>
2024-08-09 19:52:01 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,35 @@
+document.getElementById('submit-btn').addEventListener('click', async function() {
+    const symptomInput = document.getElementById('symptom-input').value;
+    if (symptomInput.trim() === '') {
+        alert('Please enter your symptoms or disease.');
+        return;
+    }
+
+    
+    try {
+        const response = await fetch('https://vedha-mo57.onrender.com/get-disease-info', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ symptoms: symptomInput })
+        });
+        
+
+        const data = await response.json();
+        if (data.error) {
+            alert(data.error);
+        } else {
+            let resultHTML = '<h2>Possible Diseases and Treatments:</h2>';
+            data.result.forEach(item => {
+                resultHTML += `<p><strong>Disease:</strong> ${item.disease_name}<br><strong>Treatment Type:</strong> ${item.treatment_type}<br><strong>Treatment:</strong> ${item.treatment_description}</p>`;
+            });
+            document.getElementById('result-box').innerHTML = resultHTML;
+        }
+    } catch (error) {
+        alert('An error occurred. Please try again later.');
+        console.error(error);
+    }
+
+    document.getElementById('symptom-input').value = '';
+});
