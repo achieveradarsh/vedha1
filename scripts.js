6aac91cc86f423e674c3348cae3faaa9035fba2f
achieveradarsh <158289007+achieveradarsh@users.noreply.github.com>
2024-08-10 11:56:39 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,6 @@ document.getElementById('submit-btn').addEventListener('click', async function()
         return;
     }
 
-    
     try {
         const response = await fetch('https://vedha-mo57.onrender.com/get-disease-info', {
             method: 'POST',
@@ -14,7 +13,6 @@ document.getElementById('submit-btn').addEventListener('click', async function()
             },
             body: JSON.stringify({ symptoms: symptomInput })
         });
-        
 
         const data = await response.json();
         if (data.error) {
@@ -22,7 +20,10 @@ document.getElementById('submit-btn').addEventListener('click', async function()
         } else {
             let resultHTML = '<h2>Possible Diseases and Treatments:</h2>';
             data.result.forEach(item => {
-                resultHTML += `<p><strong>Disease:</strong> ${item.disease_name}<br><strong>Treatment Type:</strong> ${item.treatment_type}<br><strong>Treatment:</strong> ${item.treatment_description}</p>`;
+                resultHTML += `<p><strong>Disease:</strong> ${item.disease_name}</p>`;
+                item.treatments.forEach(treatment => {
+                    resultHTML += `<p><strong>Treatment Type:</strong> ${treatment.type}<br><strong>Treatment:</strong> ${treatment.description}</p>`;
+                });
             });
             document.getElementById('result-box').innerHTML = resultHTML;
         }
